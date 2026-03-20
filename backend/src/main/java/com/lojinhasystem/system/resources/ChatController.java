9a85396b936f004a74ab317a518156cb12712d29
SaulinhoDevs <157417082+SaulinhoDevs@users.noreply.github.com>
2026-03-20 17:38:20 -0300

    implements Vertex IA no projeto

---
+++ backend/src/main/java/com/lojinhasystem/system/resources/ChatController.java
@@ -0,0 +1,25 @@
+package com.lojinhasystem.system.resources;
+
+import org.antlr.v4.runtime.misc.NotNull;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatController {
+
+    private final ChatClient chatClient;
+
+    public ChatController(@NotNull ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
+    @GetMapping("/ai")
+    String generation(String userInput) {
+        return this.chatClient.prompt()
+                .user(userInput)
+                .call()
+                .content();
+    }
+
+}
