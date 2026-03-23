2d2c4ea8f631c88e63806d09fcc91eeb18b04258
SaulinhoDevs <157417082+SaulinhoDevs@users.noreply.github.com>
2026-03-23 17:10:39 -0300

    Add chat bot UI, behavior and styles

--- frontend/src/app/ia/chat-bot/chat-bot.ts
+++ frontend/src/app/ia/chat-bot/chat-bot.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-chat-bot',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule,
+    NgClass,
+  ],
   templateUrl: './chat-bot.html',
   styleUrl: './chat-bot.css',
 })
 export class ChatBot {
+  userInput = '';
+
+  messages = signal([{ text: 'Olá! Como posso te ajudar hoje?', isBot: true }]);
+
+  sendMessage() {
+    this.trimUserMessage();
+    if (this.userInput !== '') {
+      this.updateMessages(this.userInput);
+      this.userInput = '';
+      this.simulateResponse();
+    }
+  }
+
+  private updateMessages(text: string, isBot = false){
+    this.messages.update((messages) => [...messages, { text: text, isBot: isBot }]);
+  }
+
+  private trimUserMessage() {
+    this.userInput = this.userInput.trim();
+  }
+
+  private simulateResponse() {
+    setTimeout(() => {
+      const response = 'Esta é uma resposta simulada do bot.';
+      this.updateMessages(response, true);
+    }, 2000);
+  }
 
 }
